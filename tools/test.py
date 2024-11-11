3db56bc52b4b9ebdd1fd8c838ed2555afcb2efea
adudak <adudak64a@gmail.com>
2024-11-11 16:17:16 +0200

    dependabot - test 8

--- tools/test.py
+++ tools/test.py
@@ -1,9 +1,4 @@
-# import numpy as np
-# import os
-# import urllib
+import urllib
 
-# data = [1, 2, 3, 4, 5]
-# mean_value = np.mean(data)
-# print(mean_value)  # Output: 3.0
 
-# np.source(np.mean)
+print("HELLO")
--- tools/test_import.py
+++ tools/test_import.py
@@ -1,3 +1,9 @@
+import numpy as np
+import os
 import urllib
 
-print("HELLO")
+data = [1, 2, 3, 4, 5]
+mean_value = np.mean(data)
+print(mean_value)  # Output: 3.0
+
+np.source(np.mean)
